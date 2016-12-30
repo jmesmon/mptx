037e0c596b397e2525cbac7f902fd6615fed24e6
Cody P Schafer <dev@codyps.com>
2016-12-29 22:21:46 -0500

    arduino: slight modifications to avoid warnings/errors

--- Multiprotocol/arduino/hardware/arduino/avr/cores/arduino/Arduino.h
+++ Multiprotocol/arduino/hardware/arduino/avr/cores/arduino/Arduino.h
@@ -128,7 +128,7 @@ typedef uint8_t byte;
 void init(void);
 void initVariant(void);
 
-int atexit(void (*func)()) __attribute__((weak));
+int atexit(void (*func)(void)) __attribute__((weak));
 
 void pinMode(uint8_t, uint8_t);
 void digitalWrite(uint8_t, uint8_t);
