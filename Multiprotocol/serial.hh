66835fc9d7566c9cbe411fdabde24fbf77ffe353
Cody P Schafer <dev@codyps.com>
2016-12-31 18:19:47 -0500

    serial: add definition for BASH_SERIAL
    Consider moving the resume/suspend to general items

--- Multiprotocol/serial.hh
+++ Multiprotocol/serial.hh
@@ -9,6 +9,10 @@ void Serial_write(uint8_t byte);
 bool tx_buff_has_space(void);
 void serial_reset_buf(void);
 
+#ifdef BASH_SERIAL
+void resumeBashSerial(void);
+#endif
+
 #ifdef INVERT_TELEMETRY
 # if ! defined(ORANGE_TX) && ! defined(STM32_BOARD)
 // enable bit bash for serial
